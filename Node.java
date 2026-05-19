515c54acdf93e972e190a4b1bff6e4964993d822
lucas-sobotta <sobottal@wlhs.wlwv.k12.or.us>
2026-05-19 21:45:09 +0000

    Add MyLinkedList and Node classes with basic functionality

---
+++ Node.java
@@ -0,0 +1,16 @@
+/**
+ * Node.java
+ * A single node in a linked list
+ */
+
+public class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        // This should set this.data to the given value
+        // and this.next to null
+        this.data = data;
+        this.next = null;
+    }
+}
